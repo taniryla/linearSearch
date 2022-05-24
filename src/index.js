e37071184967bb452f7948e2841cd38617e1a0e4
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-23 20:11:18 -0700

    control flow logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-function linear(array, value) {}
+function linear(array, value) {
+  for (let idx of array) {
+    if (array[idx] === value) {
+      return idx;
+    } else {
+      return -1;
+    }
+  }
+}
